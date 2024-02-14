93c7b030f25f5e033058e5b15c20b936366de2df
Fedor Urvanov <fedor@urvanov.ru>
2024-02-14 10:53:59 +0300

    Inject list and map examples

---
+++ src/main/java/ru/urvanov/virtualpets/server/example/InjectExample.java
@@ -0,0 +1,31 @@
+package ru.urvanov.virtualpets.server.example;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class InjectExample {
+    
+    private List<String> names;
+    
+    private Map<String, BigDecimal> numberSumMap;
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+
+    public Map<String, BigDecimal> getNumberSumMap() {
+        return numberSumMap;
+    }
+
+    public void setNumberSumMap(Map<String, BigDecimal> numberSumMap) {
+        this.numberSumMap = numberSumMap;
+    }
+    
+    
+
+}
