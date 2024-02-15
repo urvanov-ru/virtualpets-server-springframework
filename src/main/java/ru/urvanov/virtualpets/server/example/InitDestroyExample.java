fc7df6f92e7413acd36bf66070d1c44b33ac43e7
Fedor Urvanov <fedor@urvanov.ru>
2024-02-15 09:36:37 +0300

    Пример управления жизненным циклом бина

---
+++ src/main/java/ru/urvanov/virtualpets/server/example/InitDestroyExample.java
@@ -0,0 +1,57 @@
+package ru.urvanov.virtualpets.server.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+/**
+ * Пример управления жизненным циклом бина.
+ */
+public class InitDestroyExample implements InitializingBean, DisposableBean{
+
+    private static final Logger logger = LoggerFactory.getLogger(InitDestroyExample.class);
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Аннотации из jakarta.annotation
+    
+    @PostConstruct
+    public void jakartaAnnotationPostConstruct() {
+        logger.info("(1) @PostConstruct from jakarta.annotation");
+    }
+    
+    @PreDestroy
+    public void jakartaAnnotationPreDestroy() {
+        logger.info("(4) @PreDestroy from jakarta.annotation");
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Методы из InitializingBean и DisposableBean
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("(2) afterPropertiesSet from InitializingBean interface");
+    }
+    
+    @Override
+    public void destroy() throws Exception {
+        logger.info("(5) destroy from DisposableBean interface");
+    }
+
+
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Методы init-method из  destroy-method из XML-конфигурации бина
+    
+    public void initMethodFromXmlConfiguration() {
+        logger.info("(3) init-method from XML-configuration");
+    }
+    
+    public void destroyMethodFromXmlConfiguration() {
+        logger.info("(6) destroy-method from XML-configuration");
+    }
+
+}
