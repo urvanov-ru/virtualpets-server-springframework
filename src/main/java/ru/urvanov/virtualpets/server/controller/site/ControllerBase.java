f92d178a86f16ccb1821a5dec7cc09d2acc018a7
Fedor Urvanov <fedor@urvanov.ru>
2024-07-16 16:48:06 +0300

    Стиль кодирования ControllerBase для книги

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/ControllerBase.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/ControllerBase.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public class ControllerBase {
-    
+
     @ModelAttribute("menu_play_url")
-    public String menuPlayUrl(@Value("${virtualpets-server-springframework.play.url}") String playUrl) {
+    public String menuPlayUrl(
+            @Value("${virtualpets-server-springframework.play.url}")
+            String playUrl) {
         return playUrl;
     }
 
