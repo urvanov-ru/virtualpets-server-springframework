97802d8233682692c9aead48b61056a1d8662fdd
Fedor Urvanov <fedor@urvanov.ru>
2024-07-17 09:17:37 +0300

    Стиль кодирования для книги

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
@@ -13,9 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 @RequestMapping("site")
 public class HomeController extends ControllerBase {
@@ -23,23 +20,19 @@ public class HomeController extends ControllerBase {
     private static final Logger logger = LoggerFactory
             .getLogger(HomeController.class);
 
-
-    /**
-     * Simply selects the home view to render by returning its name.
-     */
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String home(Locale locale, Model model, HttpServletRequest request) {
+    public String home(Locale locale, Model model,
+            HttpServletRequest request) {
         logger.info("Welcome home! The client locale is {}.", locale);
         Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-                DateFormat.LONG, locale);
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(
+                DateFormat.LONG, DateFormat.LONG, locale);
 
         String formattedDate = dateFormat.format(date);
 
         model.addAttribute("serverTime", formattedDate);
-        
+
         return "home";
     }
-
 
 }
