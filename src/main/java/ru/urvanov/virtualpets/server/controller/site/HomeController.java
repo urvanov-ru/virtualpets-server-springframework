2b8b86c21bda7781e6825496586280680a7441d1
Fedor Urvanov <fedor@urvanov.ru>
2024-07-17 09:18:42 +0300

    Стиль кодирования для книги HomeController

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
@@ -1,15 +1,10 @@
 package ru.urvanov.virtualpets.server.controller.site;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,17 +16,8 @@ public class HomeController extends ControllerBase {
             .getLogger(HomeController.class);
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String home(Locale locale, Model model,
-            HttpServletRequest request) {
+    public String home(Locale locale) {
         logger.info("Welcome home! The client locale is {}.", locale);
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(
-                DateFormat.LONG, DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate);
-
         return "home";
     }
 
