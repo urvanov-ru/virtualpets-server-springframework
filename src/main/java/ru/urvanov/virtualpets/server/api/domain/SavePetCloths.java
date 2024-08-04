97b8ff4dcb50506f672cc859d151e0f5ddc8ff47
Fedor Urvanov <fedor@urvanov.ru>
2024-08-04 22:06:23 +0300

    Fix bug with SavePetCloths validation

--- src/main/java/ru/urvanov/virtualpets/server/api/domain/SavePetCloths.java
+++ src/main/java/ru/urvanov/virtualpets/server/api/domain/SavePetCloths.java
@@ -1,10 +1,9 @@
 package ru.urvanov.virtualpets.server.api.domain;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record SavePetCloths(
-        @NotNull @Size(min = 1, max = 50) String hatId,
-        @NotNull @Size(min = 1, max = 50) String clothId,
-        @NotNull @Size(min = 1, max = 50) String bowId) {
+        @Size(min = 1, max = 50) String hatId,
+        @Size(min = 1, max = 50) String clothId,
+        @Size(min = 1, max = 50) String bowId) {
 };
