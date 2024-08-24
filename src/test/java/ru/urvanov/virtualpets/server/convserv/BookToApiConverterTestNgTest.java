40653b965e946e9f0c3fac10f821b5968c4d8197
Fedor Urvanov <fedor@urvanov.ru>
2024-08-24 14:48:34 +0300

    Комментарии к тестам

--- src/test/java/ru/urvanov/virtualpets/server/convserv/BookToApiConverterTestNgTest.java
+++ src/test/java/ru/urvanov/virtualpets/server/convserv/BookToApiConverterTestNgTest.java
@@ -17,24 +17,32 @@ public class BookToApiConverterTestNgTest {
     private static final int BOOKCASE_LEVEL = 1;
     private static final String BOOK_ID = "DESTINY_BOOK";
 
+    /**
+     * Простейший пример теста TestNG.
+     */
     @Test
     void convert() {
+        // Экземпляр тестируемого класса
         BookToApiConverter converter = new BookToApiConverter();
         
+        // Подготовка исходных данных
         Book source = new Book(
                 BOOK_ID,
                 BOOKCASE_LEVEL,
                 BOOKCASE_ORDER,
                 HIDDEN_OBJECTS_GAME_DROP_RATE);
         
+        // Подготовка ожидаемого результата
         var expected = new ru.urvanov.virtualpets.server.controller.api
                 .domain.Book(
                         BOOK_ID,
                         BOOKCASE_LEVEL,
                         BOOKCASE_ORDER);
         
+        // Вызов тестируемого метода
         var actual = converter.convert(source);
         
+        // Проверка результата
         assertEquals(expected,  actual);
     }
 
