b213b74db9205ba9e7c0ee2ae820e848f7d19945
Fedor Urvanov <fedor@urvanov.ru>
2024-09-08 11:27:10 +0300

    @Primary для тестов

--- src/test/java/ru/urvanov/virtualpets/server/test/config/DaoTestConfig.java
+++ src/test/java/ru/urvanov/virtualpets/server/test/config/DaoTestConfig.java
@@ -10,11 +10,6 @@ import java.time.temporal.TemporalAccessor;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.dbunit.DataSourceDatabaseTester;
-import org.dbunit.database.DatabaseConfig;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
-import org.dbunit.util.fileloader.XlsDataFileLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,56 +23,20 @@ import org.springframework.context.annotation.Profile;
 @Profile("test")
 public class DaoTestConfig {
 
-    
-    /*@Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:prepare.sql")
-                .addScript("classpath:schema.sql").build();
-    }*/
-    
     @Bean
     public DataSource dataSource() throws IOException {
         BasicDataSource result = new BasicDataSource();
-        // result.setDriverClassName("com.mysql.jdbc.Driver");
-        //result.setUrl("jdbc:tc:mysql:8.0.33:///virtualpets?TC_INITSCRIPT=schema.sql");
         result.setUrl("jdbc:tc:postgresql:16.1:///databasename?TC_INITSCRIPT=init.sql");
-        //ClassPathResource schema = new ClassPathResource("/schema.sql");
-        //String schemaContent = "";
-        //try (InputStream inputStream = schema.getInputStream()) {
-        //    schemaContent = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-       // }
-        
-        //result.setConnectionInitSqls(List.of("GRANT ALL PRIVILEGES ON *.* TO 'test'@'%';"));
-        //result.setUsername("root");
-        //result.setPassword("");
         result.setDefaultSchema("virtualpets_server_springframework");
         return result;
     }
 
-    @Bean(name = "databaseTester")
-    public DataSourceDatabaseTester dataSourceDatabaseTester() throws Exception {
-        DataSourceDatabaseTester databaseTester = new DataSourceDatabaseTester(
-                dataSource()) {
-            public IDatabaseConnection getConnection() throws Exception {
-                IDatabaseConnection result = super.getConnection();
-                result.getConfig().setProperty(DatabaseConfig.PROPERTY_ESCAPE_PATTERN, "\"?\"");
-                result.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new PostgresqlDataTypeFactory());
-                return result;
-            }
-        };
-        return databaseTester;
-    }
-
-    @Bean(name = "xlsDataFileLoader")
-    public XlsDataFileLoader xlsDataFileLoader() {
-        return new XlsDataFileLoader();
-    }
-    
     @Bean
-    public Clock clock() {
+    @Primary
+    public Clock fixedClock() {
         ZoneId zoneId = ZoneId.of("Europe/Moscow");
-        TemporalAccessor offsetDateTime = ZonedDateTime.of(2024, 3, 15, 18, 52, 0, 0, zoneId);
+        TemporalAccessor offsetDateTime = ZonedDateTime.of(
+                2024, 3, 15, 18, 52, 0, 0, zoneId);
         Instant instant = Instant.from(offsetDateTime );
         return Clock.fixed(instant, zoneId);
     }
