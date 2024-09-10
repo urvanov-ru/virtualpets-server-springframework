655eff6cde2a1afb16df4d219d340795848b3ecb
Fedor Urvanov <fedor@urvanov.ru>
2024-09-10 16:44:27 +0300

    SingleConnectionDataSource

--- src/test/java/ru/urvanov/virtualpets/server/dao/DaoTestConfig.java
+++ src/test/java/ru/urvanov/virtualpets/server/dao/DaoTestConfig.java
@@ -1,5 +1,7 @@
 package ru.urvanov.virtualpets.server.dao;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -8,13 +10,13 @@ import java.time.temporal.TemporalAccessor;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 /**
  * Конфигурация Spring для тестирования слоя DAO отдельно от всех
@@ -27,17 +29,38 @@ import org.springframework.context.annotation.Profile;
 public class DaoTestConfig {
 
     /**
-     * Настраивает источник данных, подключающийся к базе данных
+     * Настройка источника данных на базу данных
      * PostgreSQL в контейнере Testcontainers.
      * @return Источник данных для тестов.
+     * @throws SQLException 
      */
-    @Bean
-    public DataSource dataSource() {
-        BasicDataSource result = new BasicDataSource();
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() throws IOException, SQLException {
+        // DataSource с одним всегда работающим подключением.
+        // Фактически это не пул подключений, он всегда
+        // возвращает одно и то же подключение к базе данных,
+        // не создавая новое.
+        SingleConnectionDataSource result = new SingleConnectionDataSource();
+        
+        // jdbc:tc ... - это адрес к базе данных в контейнере
+        // Адрес взят из документации Testcontainers.
         result.setUrl("""
                 jdbc:tc:postgresql:16.1:///databasename?\
                 TC_INITSCRIPT=init.sql""");
-        result.setDefaultSchema("virtualpets_server_springframework");
+        
+        // ContainerDatabaseDriver работает с подключениями вида
+        // jdbc:tc
+        result.setDriverClassName(
+                "org.testcontainers.jdbc.ContainerDatabaseDriver");
+        
+        // Рабочая схема с таблицами проекта.
+        result.setSchema("virtualpets_server_springboot");
+        
+        // Заглушаем вызовы close в возвращаемом подключении, 
+        // чтобы никто не закрывал наше единственное подключение.
+        // В реальности это оборачивает возвращаемый Connection
+        // в прокси, в котором вызовы close игнорируются.
+        result.setSuppressClose(true);
         return result;
     }
 
