0cd4767fa7c2fdffcdd5a5c51a6f41b382e6cffc
Fedor Urvanov <fedor@urvanov.ru>
2024-09-10 10:21:56 +0300

    Стиль кодирования для книги

--- src/test/java/ru/urvanov/virtualpets/server/controller/api/BaseControllerTest.java
+++ src/test/java/ru/urvanov/virtualpets/server/controller/api/BaseControllerTest.java
@@ -12,14 +12,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import ru.urvanov.virtualpets.server.test.config.MockMvcConfig;
 
 /**
